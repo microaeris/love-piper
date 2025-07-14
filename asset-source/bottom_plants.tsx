f96bb2a81c9957708ef49576c0857525b7548a36
shoe <austinshyu@gmail.com>
2025-07-13 17:05:58 -0700

    actually add the asset-source and assets file

---
+++ asset-source/bottom_plants.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="bottom_plants" tilewidth="8" tileheight="8" tilecount="360" columns="20">
+ <image source="../assets/images/bottom_plants.png" width="160" height="144"/>
+</tileset>
